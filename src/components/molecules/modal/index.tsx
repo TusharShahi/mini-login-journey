e215e38cbd737c9bf0cb60ce50de9415f2f5bb9d
Tushar Shahi <tusharshahi@MacBook-Pro.local>
2024-07-02 02:56:39 +0530

    escaping close

--- src/components/molecules/modal/index.tsx
+++ src/components/molecules/modal/index.tsx
@@ -2,6 +2,7 @@ import ReactDOM from "react-dom";
 import styles from "./styles.module.css";
 import IconButton from "../../atoms/buttons/icon";
 import useFocusTrap from "../../../hooks/use-focus-trap";
+import { useEffect } from "react";
 
 interface ModalProps {
   children: React.ReactNode;
@@ -17,8 +18,22 @@ const Modal: React.FC<ModalProps> = ({
 }) => {
   const modalRef = useFocusTrap(true, controlFirstFocus);
 
+  useEffect(() => {
+    const handleKeyDown = (event: KeyboardEvent) => {
+      if (event.key === "Escape") {
+        onClose();
+      }
+    };
+
+    document.addEventListener("keydown", handleKeyDown);
+
+    return () => {
+      document.addEventListener("keydown", handleKeyDown);
+    };
+  }, [onClose]);
+
   return ReactDOM.createPortal(
-    <div className={styles.modal} ref={modalRef}>
+    <div aria-modal="true" className={styles.modal} ref={modalRef}>
       <div>
         <IconButton onClick={onClose}>x</IconButton>
         {children}
